5320ced67d4c24eb43be877fcb3a1b902f888801
yooerizkilab <yooerizki10@webmail.umm.ac.id>
2021-04-28 02:41:50 +0700

    edit title

--- application/views/frontend/layout/header.php
+++ application/views/frontend/layout/header.php
@@ -2,33 +2,33 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>Mamba Bootstrap Template - Index</title>
-    <meta content="" name="description">
-    <meta content="" name="keywords">
+  <title>Company</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
 
-    <!-- Favicons -->
-    <link href="<?= base_url('assets/front/'); ?>img/favicon.png" rel="icon">
-    <link href="<?= base_url('assets/front/'); ?>img/apple-touch-icon.png" rel="apple-touch-icon">
+  <!-- Favicons -->
+  <link href="<?= base_url('assets/front/'); ?>img/favicon.png" rel="icon">
+  <link href="<?= base_url('assets/front/'); ?>img/apple-touch-icon.png" rel="apple-touch-icon">
 
-    <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,600,600i,700,700i,900" rel="stylesheet">
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,600,600i,700,700i,900" rel="stylesheet">
 
-    <!-- Vendor CSS Files -->
-    <link href="<?= base_url('assets/front/'); ?>vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="<?= base_url('assets/front/'); ?>vendor/icofont/icofont.min.css" rel="stylesheet">
-    <link href="<?= base_url('assets/front/'); ?>vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-    <link href="<?= base_url('assets/front/'); ?>vendor/animate.css/animate.min.css" rel="stylesheet">
-    <link href="<?= base_url('assets/front/'); ?>vendor/venobox/venobox.css" rel="stylesheet">
-    <link href="<?= base_url('assets/front/'); ?>vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
-    <link href="<?= base_url('assets/front/'); ?>vendor/aos/aos.css" rel="stylesheet">
+  <!-- Vendor CSS Files -->
+  <link href="<?= base_url('assets/front/'); ?>vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?= base_url('assets/front/'); ?>vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="<?= base_url('assets/front/'); ?>vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="<?= base_url('assets/front/'); ?>vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="<?= base_url('assets/front/'); ?>vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="<?= base_url('assets/front/'); ?>vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="<?= base_url('assets/front/'); ?>vendor/aos/aos.css" rel="stylesheet">
 
-    <!-- Template Main CSS File -->
-    <link href="<?= base_url('assets/front/'); ?>css/style.css" rel="stylesheet">
+  <!-- Template Main CSS File -->
+  <link href="<?= base_url('assets/front/'); ?>css/style.css" rel="stylesheet">
 
-    <!-- =======================================================
+  <!-- =======================================================
   * Template Name: Mamba - v2.5.1
   * Template URL: https://bootstrapmade.com/mamba-one-page-bootstrap-template-free/
   * Author: BootstrapMade.com
